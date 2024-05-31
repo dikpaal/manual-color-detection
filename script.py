492e3422360a5a0a5c7950db2a2e695b9ffc9d0d
dikpaal <dikpaal@icloud.com>
2024-05-31 11:47:21 +0530

    the script is functional

---
+++ script.py
@@ -0,0 +1,27 @@
+# first !pip install pillow
+
+from PIL import Image
+import numpy as np
+from collections import Counter
+
+def image_to_rgb_array(image_path):
+    
+    with Image.open(image_path) as img:
+        img = img.convert('RGB')
+        img_array = np.array(img)
+    return img_array
+
+def get_top_colors(image_path, top_n=5):
+
+    rgb_array = image_to_rgb_array(image_path)
+    reshaped_array = rgb_array.reshape(-1, 3)
+    color_counts = Counter(map(tuple, reshaped_array))
+    top_colors = color_counts.most_common(top_n)
+    
+    return top_colors
+
+image_path = 'images/ubcor.png'
+top_colors = get_top_colors(image_path)
+print("Top 5 most appearing colors (in RGB):")
+for color, count in top_colors:
+    print(f"Color: {color}, Count: {count}")``
