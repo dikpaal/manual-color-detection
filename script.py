06f431b67d0016f46c25910b5d30476d6d5b48ba
dikpaal <dikpaal@icloud.com>
2024-07-06 14:08:48 -0700

    dataset

--- script.py
+++ script.py
@@ -1,31 +1,32 @@
-# !pip3 install pillow
-# !pip3 install webcolors
-
 from PIL import Image
 import numpy as np
 from collections import Counter
 import webcolors
 
-# Converts the image into 3D array of pixels
 def image_to_rgb_array(image_path):
-
+    # Open the image file
     with Image.open(image_path) as img:
+        # Convert the image to RGB mode
         img = img.convert('RGB')
+        # Convert the image to a NumPy array
         img_array = np.array(img)
-
     return img_array
 
-# Classifies the top 5 most appearing pixels
 def get_top_colors(image_path, top_n=5):
-    
+    # Get the 3D array of RGB values
     rgb_array = image_to_rgb_array(image_path)
+    
+    # Reshape the array to a 2D array where each row is a color
     reshaped_array = rgb_array.reshape(-1, 3)
+    
+    # Count the occurrence of each color
     color_counts = Counter(map(tuple, reshaped_array))
+    
+    # Get the top N most common colors
     top_colors = color_counts.most_common(top_n)
     
     return top_colors
 
-# Classifies inputted pixel into the nearest color
 def closest_color(requested_color):
     min_colors = {}
     for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
@@ -36,7 +37,6 @@ def closest_color(requested_color):
         min_colors[(rd + gd + bd)] = name
     return min_colors[min(min_colors.keys())]
 
-# Classifies inputted pixel into the nearest color
 def get_color_name(requested_color):
     try:
         color_name = webcolors.rgb_to_name(requested_color)
@@ -44,20 +44,23 @@ def get_color_name(requested_color):
         color_name = closest_color(requested_color)
     return color_name
 
-# Main function
 def classify_top_colors(image_path, top_n=5):
     top_colors = get_top_colors(image_path, top_n)
     classified_colors = [(color, count, get_color_name(color)) for color, count in top_colors]
     
     return classified_colors
 
+def main():
+    image_path = './images/bottle.png'  # Replace with the path to your image
+    user_input = int(input("Enter the number of top colors to return (1 or 2): "))
+    if user_input != 1 and user_input != 2:
+        print("Please enter 1 or 2.")
+        return
 
-image_path = 'images/red_image.jpg'
-classified_colors = classify_top_colors(image_path)
-print("Top colors classified (in order of occurrence):")
-for color, count, classification in classified_colors:
-    print(f"Color: {color}, Count: {count}, Classified as: {classification}")
+    classified_colors = classify_top_colors(image_path, user_input)
+    print(f"Top {user_input} colors classified (in order of occurrence):")
+    for color, count, classification in classified_colors:
+        print(f"Color: {color}, Count: {count}, Classified as: {classification}")
 
-
-print()
-print("Final result: " + classified_colors[0][2])
+if __name__ == "__main__":
+    main()
